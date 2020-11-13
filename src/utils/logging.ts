bc254f76ab9416f593db3c500a7f86da91440dc1
Jan Löbel <jan.loebel@gmail.com>
2020-11-13 14:02:47 +0100

    Fixed wrong usage of debug

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,11 +1,11 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const DEBUG = (process.env.DEBUG as string).toLowerCase() === 'true';
+const DEBUG = (process.env.DEBUG as string)?.toLowerCase() === 'true';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const LOG = (...msg: any): void => {
-  if (DEBUG) {
+  if (!!DEBUG) {
     console.log(...msg);
   }
 };
