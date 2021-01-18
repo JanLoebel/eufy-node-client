ced235f3d941a22467d274bd8ea1759587306cc3
Jan Löbel <jan.loebel@gmail.com>
2021-01-18 22:39:26 +0100

    Added CloudLookupService to exports

--- src/p2p/index.ts
+++ src/p2p/index.ts
@@ -1,3 +1,4 @@
+export { CloudLookupService } from './cloud-lookup.service';
 export { LocalLookupService } from './local-lookup.service';
 export { DeviceClientService } from './device-client.service';
 export { CommandType } from './command.model';
